c074490914519a00e5e044ab8596cbdf7bf4b1f6
Rohit Rahul <therohitrahul@gmail.com>
2022-11-13 13:36:33 +0530

    added docker installation

--- readme.md
+++ readme.md
@@ -1,4 +1,19 @@
-Scripts for installation of various libraries
+These scripts will setup a new installation of ubuntu with all the programs and libraries that are required
+
+The utilities include
+
+Docker
+VLC
+GIMP
+Darktable
+Audacity
+
+The installation of these programs are present in the script util_install.sh
+
+To run util_install.sh :
+
+chmod +x util_install.sh
+./util_install.sh
 
 Opencv
 
@@ -6,4 +21,6 @@ Steps :
     1. chmod +x install_opencv.sh
     2. ./install_opencv.sh
 
-Note : This currently works for python 3 and also creates c++ libraries 
+Note : This currently works for python 3 and also creates c++ libraries
+
+Please note that you may have to intervene during installation
--- util_install.sh
+++ util_install.sh
@@ -2,11 +2,11 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
-sudo apt install sox
+sudo apt -y install sox
 
 sudo apt -y install htop
 sudo apt -y install iotop
-sudo apt install net-tools
+sudo apt -y install net-tools
 
 sudo apt -y install ffmpeg
 
@@ -14,3 +14,17 @@ sudo apt -y install gimp
 sudo apt -y install vlc
 sudo apt -y install darktable
 sudo apt -y install audacity 
+
+# Installing Docker 
+sudo apt -y install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt -y update
+
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker run hello-world
